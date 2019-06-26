77fcd09ab5c31e51d45b9f56882f67ec0f60a453
Paulo Barros <paulo.alfredo.barros@gmail.com>
2019-06-26 10:02:59 -0300

    feat(auth): Routing modules

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,71 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Root from './views/Root.vue'
+
+// store
+import store from '@/store/index'
+
+// Layouts
+import Auth from '@/views/_layouts/Auth.vue'
+import UnAuth from '@/views/_layouts/UnAuth.vue'
+
+// Views
+import Dashboard from '@/views/Dashboard.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const authRoutes = [{
+  path: '',
+  component: Auth,
+  children: [{
+    path: '/',
+    name: 'dashboard',
+    component: Dashboard,
+    meta: { title: 'Dashboard', requireAuth: true }
+  }]
+}]
+
+const unAuthRoutes = [{
+  path: '',
+  component: UnAuth,
+  children: [{
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: { title: 'Login', requireAuth: false }
+  }]
+}]
+
+// Create router
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'root',
-      component: Root
-    }
+    ...authRoutes,
+    ...unAuthRoutes
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // Custom title for page, if exists
+  // otherwise use default 'Transparência na Pesagem'
+  document.title = `Deep Throught | ${to.meta.title || ''}`
+
+  // get current route
+  let nextTo = from.name
+
+  // Check if route requires authentication
+  // If it requires, redirect to Login
+  // If not, let to pass
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    // Check if user is not authenticated
+    if (!store.getters.isAuthenticated) {
+      // change route to login
+      nextTo = 'login'
+    }
+  }
+  // go to route
+  next(nextTo)
+})
+
+export default router
