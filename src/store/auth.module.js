56bee93ab7ad90e8d27e8376bf654a5ea6cb34e6
Paulo Barros <paulo.alfredo.barros@gmail.com>
2019-07-08 22:24:42 -0300

    feat(gauth): Add GoogleAuthService

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -1,68 +1,47 @@
-import {
-  REMOVE_JWT_TOKEN,
-  SET_JWT_TOKEN,
-  LOGIN,
-  AUTO_LOGIN,
-  LOGOUT
-} from './auth.actions'
-import JwtService from '@/services/jwt'
-
 import ApiClient from '../client/api'
-
-import AuthService from '../services/auth'
+import JwtService from '../services/jwt'
+import GoogleAuthService from '../services/google-auth'
+import { AUTO_LOGIN, LOGIN, LOGOUT, REMOVE_JWT_TOKEN, SET_JWT_TOKEN } from './auth.actions'
 
 const state = {
   authUser: undefined
 }
 
+// todo: should be removed
+const FIXTURE_USER = {
+  user: {
+    'id': 'nice-id',
+    'name': 'Bob Cat',
+    'email': 'bob@cat.meow',
+    'picture': 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000108//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100',
+    'verified_email': true,
+    'created_at': '2019-06-22T22:31:11+00:00',
+    'updated_at': '2019-06-22T22:31:12+00:00'
+  },
+  token: 'fake-token'
+}
+
 const getters = {
   authUser: () => state.authUser,
-  isAuthenticated: () => !!state.authUser && !!JwtService.getToken()
+  isAuthenticated: () => !!state.authUser && !!JwtService.getToken(),
+  authSingInUrl: () => GoogleAuthService.singInUrl()
 }
 
 const actions = {
-  async [LOGIN] ({ commit }) {
-    // todo: change callback to real api with gauth
-
-    const { user, token } = await AuthService.login()
-
-    // const { user, token } = {
-    //   user: {
-    //     'id': 'nice-id',
-    //     'name': 'Bob Cat',
-    //     'email': 'bob@cat.meow',
-    //     'picture': 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000108//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100',
-    //     'verified_email': true,
-    //     'created_at': '2019-06-22T22:31:11+00:00',
-    //     'updated_at': '2019-06-22T22:31:12+00:00'
-    //   },
-    //   token: 'fake-token'
-    // }
-    commit(SET_JWT_TOKEN, { user, token })
-  },
-  [REMOVE_JWT_TOKEN] ({ commit }) {
-    commit(REMOVE_JWT_TOKEN)
-  },
   [AUTO_LOGIN] ({ commit }) {
     // todo: change callback to real api with gauth
     if (JwtService.getToken()) {
-      const { user, token } = {
-        user: {
-          'id': 'nice-id',
-          'name': 'Bob Cat',
-          'email': 'bob@cat.meow',
-          'picture': 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000108//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100',
-          'verified_email': true,
-          'created_at': '2019-06-22T22:31:11+00:00',
-          'updated_at': '2019-06-22T22:31:12+00:00'
-        },
-        token: 'fake-token'
-      }
+      const { user, token } = FIXTURE_USER
       commit(SET_JWT_TOKEN, { user, token })
     }
   },
   [LOGOUT] ({ commit }) {
     commit(REMOVE_JWT_TOKEN)
+  },
+  [LOGIN] ({ commit }) {
+    // todo: change callback to real api with gauth
+    const { user, token } = FIXTURE_USER
+    commit(SET_JWT_TOKEN, { user, token })
   }
 }
 
